62fa6abd0b05c004e83d9553737213a726339391
dimasaf_ <dimas.firmana@ist.id>
2025-01-29 20:10:35 +0700

    feat: test vercel deployment

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const serverless = require("serverless-http"); // Add this
 const apiRoutes = require("./api/index");
 
 const app = express();
-const PORT = 3030;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use((err, req, res, next) => {
+  console.error("Error occurred:", err.stack);
+  res.status(500).json({ status: "99", message: "Internal Server Error" });
+});
+
 app.use("/api", apiRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Mock service is running on http://localhost:${PORT}`);
-});
+module.exports.handler = serverless(app);
+module.exports.app = app;
